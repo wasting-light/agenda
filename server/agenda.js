96aaf8eb91c1dcdd06fc79fd4eda405e700c2429
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-05-19 09:31:17 -0300

    Add comments to agenda.js

--- server/agenda.js
+++ server/agenda.js
@@ -1,5 +1,9 @@
+/**
+ * Module dependencies
+ */
+
 var bodyParser     = require('body-parser');
-var config = require('./config');
+var config         = require('./config');
 var debug          = require('debug');
 var errorHandler   = require('errorhandler');
 var express        = require('express');
@@ -11,22 +15,49 @@ var mongoose       = require('mongoose');
 var morgan         = require('morgan');
 var path           = require('path');
 
+/**
+ * Application prototype
+ */
+
 var app = express();
 
+/**
+ * Config
+ */
+
 app.set('port', process.env.PORT || config.port);
+app.set('public folder', path.join(__dirname, '../public'));
+
+/**
+ * Middleware
+ */
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride());
+app.use(express.static(app.get('public folder')));
 
-var publicFolder = path.join(__dirname, '../public');
-app.use(express.static(publicFolder));
+/**
+ * Environment
+ */
 
 var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
   app.use(errorHandler());
 }
+
+/**
+ * Routes
+ */
+
+app.get('/', function(req, res) {
+  res.json(app.get('public folder'));
+})
+
+/**
+ * Bootstrap
+ */
 
 app.listen(app.get('port'));
