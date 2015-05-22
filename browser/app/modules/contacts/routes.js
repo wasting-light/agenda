d6eb804520641dd093720c6b55d18a8988d5b8f5
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-05-21 23:52:09 -0300

    Protect the views

--- browser/app/modules/contacts/routes.js
+++ browser/app/modules/contacts/routes.js
@@ -23,29 +23,60 @@
       url: '/contacts/',
       templateUrl: 'views/contacts/list.html',
       controller: 'ContactsListController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      resolve: {
+        authenticated: authenticationResolve
+      }
     });
 
     $stateProvider.state('contacts-create', {
       url: '/contacts/create/',
       templateUrl: 'views/contacts/create.html',
       controller: 'ContactsCreateController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      resolve: {
+        authenticated: authenticationResolve
+      }
     });
 
     $stateProvider.state('contacts-show', {
       url: '/contacts/:id/',
       templateUrl: 'views/contacts/show.html',
       controller: 'ContactsShowController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      resolve: {
+        authenticated: authenticationResolve
+      }
     });
 
     $stateProvider.state('contacts-edit', {
       url: '/contacts/:id/edit/',
       templateUrl: 'views/contacts/edit.html',
       controller: 'ContactsEditController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      resolve: {
+        authenticated: authenticationResolve
+      }
     });
+  }
+
+  /**
+   * Handle authenticated and unauthenticated users
+   *
+   * @ngInject
+   * @api private
+   */
+
+  function authenticationResolve($q, $location, $auth) {
+    var defer = $q.defer();
+
+    if(!$auth.isAuthenticated()) {
+      $location.path('/login');
+    } else {
+      defer.resolve();
+    }
+
+    return defer.promise;
   }
 
 })(angular);
